0bcc22c6e91cbed7fd9c0ef6d37b27d188fe9f6a
DIMAS MAHESA <96423520+Dimjol@users.noreply.github.com>
2024-07-11 22:07:39 +0700

    SOAP API Automation Katalon

--- Object Repository/CountryInfoCountryCurrency.rs
+++ Object Repository/CountryInfoCountryCurrency.rs
@@ -53,6 +53,15 @@ import internal.GlobalVariable as GlobalVariable
 
 RequestObject request = WSResponseManager.getInstance().getCurrentRequest()
 
-ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()</verificationScript>
+ResponseObject response = WSResponseManager.getInstance().getCurrentResponse()
+
+
+WS.verifyResponseStatusCode(response, 200)
+
+assertThat(response.getStatusCode()).isEqualTo(200)
+
+WS.verifyElementText(response, 'CountryCurrencyResponse.CountryCurrencyResult.sISOCode', 'USD')
+
+WS.verifyElementText(response, 'CountryCurrencyResponse.CountryCurrencyResult.sName', 'Dollars')</verificationScript>
    <wsdlAddress>http://webservices.oorsprong.org/websamples.countryinfo/CountryInfoService.wso?WSDL</wsdlAddress>
 </WebServiceRequestEntity>
